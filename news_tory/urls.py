066c167d6d0808d8b2cc216b3274142d4e918698
Leespect <wewqwew153@gmail.com>
2023-07-24 18:53:01 +0900

    add path in news_tory urls

--- news_tory/urls.py
+++ news_tory/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('articles/', include('articles.urls')),
     path('dj/', include('dj_rest_auth.urls')),
     path('dj/registration/', include('dj_rest_auth.registration.urls')),
     # path('accounts/', include('allauth.urls')),
